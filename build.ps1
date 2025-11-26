e83124e0a6972e4c50c671bb2c33cb36ac2b0f9c
Springcomp <springcomp@users.noreply.github.com>
2025-11-26 21:30:54 +0100

    QWERTY

--- build.ps1
+++ build.ps1
@@ -17,8 +17,8 @@ PROCESS {
 
   if ($release.IsPresent){
     . ./gradlew.bat clean assembleRelease
-    . "$($Env:ANDROID_HOME)/build-tools/$($version)/zipalign.exe" -v -p 4 ./app/build/outputs/apk\release/springcomp.keyboard-layouts.azerty-nf-1.0.apk ./app/build/outputs/apk/release/app-release-aligned.apk
-    . "$($Env:ANDROID_HOME)/build-tools/$($version)/apksigner.bat" sign --ks ./app/keystore/release.jks --ks-key-alias release --ks-pass pass:android --key-pass pass:android --out ./app/build/outputs/apk/release/app-release-signed.apk ./app/build/outputs/apk/release/app-release-aligned.apk
-    . "$($Env:ANDROID_HOME)/build-tools/$($version)/apksigner.bat" verify --verbose ./app/build/outputs/apk/release/app-release-signed.apk
+    . "$($Env:ANDROID_HOME)/build-tools/$($version)/zipalign.exe" -v -p 4 ./app/build/outputs/apk\release/springcomp.keyboard-layouts.azerty-nf-1.0.apk ./app/build/outputs/apk/release/springcomp.keyboard-layouts.azerty-nf-aligned.apk
+    . "$($Env:ANDROID_HOME)/build-tools/$($version)/apksigner.bat" sign --ks ./app/keystore/release.jks --ks-key-alias release --ks-pass pass:android --key-pass pass:android --out ./app/build/outputs/apk/release/springcomp.keyboard-layouts.azerty-nf-signed.apk ./app/build/outputs/apk/release/springcomp.keyboard-layouts.azerty-nf-aligned.apk
+    . "$($Env:ANDROID_HOME)/build-tools/$($version)/apksigner.bat" verify --verbose ./app/build/outputs/apk/release/springcomp.keyboard-layouts.azerty-nf-signed.apk
   }
 }
